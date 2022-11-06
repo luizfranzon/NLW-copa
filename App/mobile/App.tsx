b657ea1ba184f3049d2a097ebe7199833363af7b
Luiz <luizfranzon05@hotmail.com>
2022-11-06 13:00:48 -0300

    Aula 04 - Conectando o app mobile à API

--- App/mobile/App.tsx
+++ App/mobile/App.tsx
@@ -1,26 +1,15 @@
 import { NativeBaseProvider, StatusBar } from "native-base";
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_500Medium,
-  Roboto_700Bold,
-} from "@expo-google-fonts/roboto";
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold} from "@expo-google-fonts/roboto";
 
 import { Loading } from "./src/components/Loading";
 
 import { THEME } from "./src/styles/theme";
-import { SignIn } from "./src/screens/SignIn";
 import { AuthContextProvider } from "./src/context/AuthContext";
-import { New } from "./src/screens/New";
-import { Find } from "./src/screens/Find";
-import { Pools } from "./src/screens/Pools";
+
+import { Routes } from "./src/routes";
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    Roboto_400Regular,
-    Roboto_500Medium,
-    Roboto_700Bold,
-  });
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
 
   return (
     <NativeBaseProvider theme={THEME}>
@@ -30,7 +19,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        {fontsLoaded ? <Pools /> : <Loading />}
+        {fontsLoaded ? <Routes /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
--- App/mobile/src/screens/SignIn.tsx
+++ App/mobile/src/screens/SignIn.tsx
@@ -6,7 +6,7 @@ import Logo from "../assets/logo.svg";
 import { Button } from "../components/Buttons";
 
 export function SignIn() {
-  const { signIn, user } = useAuth();
+  const { signIn, isUserLoading } = useAuth();
 
   return (
     <Center flex={1} bgColor="gray.900" p={7}>
@@ -15,8 +15,10 @@ export function SignIn() {
         type="SECONDARY"
         title="ENTRAR COM GOOGLE"
         leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
-        mt={12}
         onPress={signIn}
+        mt={12}
+        isLoading={isUserLoading}
+        _loading={{_spinner: {color: "white"}}}
       />
       <Text color="white" textAlign="center" mt={4}>
         Não utilizamos nenhuma informação além {"\n"} do seu e-mail para criação
